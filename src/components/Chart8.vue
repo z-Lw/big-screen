9b851532371d84528a905347dc09372e9c1fd4c4
z-Lw <553870855@qq.com>
2022-05-22 15:19:51 +0800

    完成chart8

--- src/components/Chart8.vue
+++ src/components/Chart8.vue
@@ -1,7 +1,15 @@
 <template>
-    <div class="年龄段-图2">8
-        <div ref=chart class="chart">
-
+    <div class="年龄段-图2">
+        <div class="chart">
+            <div class="main" ref="chart"/>
+                <div class="text">年龄段</div>
+        </div>
+        <div class="legend">
+            <span :style="{background: colors[0]}" />10-20
+            <span :style="{background: colors[1]}" />20-30
+            <span :style="{background: colors[2]}" />30-40
+            <span :style="{background: colors[3]}"/> 40-50
+            <span :style="{background: colors[4]}" />50-60
         </div>
     </div>
 </template>
@@ -13,38 +21,89 @@
   import global from '@/global';
   @Component
   export default class Chart8 extends Vue {
-
+    colors = ['#856BED', '#F46064', '#F38E1C', '#1CDB7C', '#33A4FA'];
+    mounted(){
+      const {colors} = this
+      const myChart = echarts.init(this.$refs.chart as HTMLDivElement);
+      myChart.setOption(global.createEchartsOptions({
+        color: colors,
+        xAxis: {show: false},
+        yAxis: {show: false},
+        legend: {show: false},
+        series: [
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius: ['75%', '90%'],
+            avoidLabelOverlap: false,
+            label: {
+              show: true, position: 'inside', textStyle: {color: 'white', fontSize: global.px(20)},
+              formatter(options: { value: number; }) {
+                return (options.value * 100).toFixed(0) + '%';
+              }
+            },
+            labelLine: {show: false},
+            itemStyle: {
+              borderColor: '#0F113A',
+              borderWidth: global.px(4)
+            },
+            data: [
+              {value: 0.07, name: '10-20'},
+              {value: 0.10, name: '20-30'},
+              {value: 0.23, name: '30-40'},
+              {value: 0.28, name: '40-50'},
+              {value: 0.32, name: '50-60'},
+            ]
+          }
+        ]
+      }));
+    }
   }
 </script>
 
 <style lang="scss" scoped>
     @import '../index.scss';
-    .年龄段 {
+    .年龄段-图2 {
+        flex: 1;
         display: flex;
         flex-direction: column;
-        height: px(363);
-        align-items: center;
-        h2 {
-            flex-shrink: 0;
-            border: 1px solid #0a5299;
-            border-bottom-left-radius: 4px;
-            border-bottom-right-radius: 4px;
-            font-size: px(22);
-            line-height: px(24);
-            padding: px(10) px(28);
-            text-shadow: 0 0 px(3) white;
+        width: px(290);
+        flex-shrink: 0;
+        .legend {
+            height: px(40);
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+            flex-wrap: wrap;
+            > span {
+                display: inline-block;
+                width: px(16);
+                height: px(10);
+                border-radius: px(2);
+                margin: 0 px(10);}
+            .male {background: #33a4fa;}
+            .female {background: #8d70f8;}
         }
-        > .charts {
+        .chart {
+            position: relative;
+            display: flex;
+            justify-content: center;
+            flex: 1;
             width: 100%;
-            flex: 1;
-            display: flex;
-            padding: px(30);
-        }
-        &-图2 {
-            flex: 1;
-            border: 1px solid red;
-            display: flex;
-            flex-direction: column;
+            > .main {
+                flex: 1;
+            }
+            > .text {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                color: #33a4fa;
+                font-size: px(40);
+                text-shadow: 0 0 px(20) #33a4fa;
+                font-weight: bold;
+            }
         }
     }
+
 </style>
